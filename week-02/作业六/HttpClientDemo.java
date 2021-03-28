2ed905114f0a541087788bc177fb0cccf4613bb0
GreenHuo <65143096+GreenHuo@users.noreply.github.com>
2021-03-28 22:21:21 +0800

    Add files via upload

---
+++ week-02/作业六/HttpClientDemo.java
@@ -0,0 +1,57 @@
+package com.larch.demo.httpclientdemo;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+/**
+ * @author ll
+ * @create 2021-03-28 21:51
+ */
+public class HttpClientDemo {
+    public static void main(String[] args) {
+        HttpClientDemo httpClientDemo = new HttpClientDemo();
+        httpClientDemo.get();
+    }
+
+    public void get() {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            // 创建httpget.
+            HttpGet httpget = new HttpGet("http://localhost:8801/HttpClientDemo");
+            System.out.println("executing request " + httpget.getURI());
+            // 执行get请求.
+            CloseableHttpResponse response = httpclient.execute(httpget);
+            try {
+                // 获取响应实体
+                HttpEntity entity = response.getEntity();
+                // 打印响应状态
+                System.out.println(response.getStatusLine());
+                if (entity != null) {
+                    // 打印响应内容长度
+                    System.out.println("Response content length: " + entity.getContentLength());
+                    // 打印响应内容
+                    System.out.println("Response content: " + EntityUtils.toString(entity));
+                }
+            } finally {
+                response.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭连接,释放资源
+            try {
+                httpclient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
